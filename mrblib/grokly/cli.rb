efec3cc46ea9e2e62fe174d1bdeec450e0e051a7
Strech (Sergey Fedorov) <oni.strech@gmail.com>
2015-12-20 21:57:25 +0100

    Created command help

--- mrblib/grokly/cli.rb
+++ mrblib/grokly/cli.rb
@@ -3,26 +3,51 @@ module Grokly
     SHORT_OPTIONS = "d:e:".freeze
     LONG_OPTIONS = %w[version help].map { |x| x.freeze }
 
-    def initialize(argv)
-      @options = extract_options(argv)
-      @examples = read_examples($stdin)
+    def initialize(args)
+      @options = extract_options(args)
+      @sentence = extract_sentence(args)
     end
 
     def run
-      puts "@options = #{@options}"
-      puts "@examples = #{@examples}"
+      #puts "@options = #{@options.inspect}"
+      #puts "@sentence = #{@sentence.inspect}"
+
+      if help?
+        Grokly::Commands::Help.new.run
+      elsif compile_and_test?
+        Grokly::Commands::Test.new(sentence, examples: examples, dictionary: dictionary).run
+      elsif compile?
+        Grokly::Commands::Compile.new(sentence, dictionary: dictionary).run
+      end
     end
 
     private
 
     def extract_options(args)
       args.class.send(:include, Getopts)
-      args.getopts(SHORT_OPTIONS, LONG_OPTIONS)
+      args.getopts(SHORT_OPTIONS, *LONG_OPTIONS)
     end
 
-    def read_examples(io)
-      return [] if IO.select([io], [], [], 0.001).nil?
-      io.readlines
+    def extract_sentence(args)
+      args.last if /\A[^\-]/ =~ args.last
+    end
+
+    def help?
+      !!@options["?"] || !!@options["help"] || @sentence.nil?
+    end
+
+    def compile?
+    end
+
+    def compile_and_test?
+    end
+
+    def dictionary
+    end
+
+    def examples
+      IO.select([$stdin], [], [], 0.001).nil?
+      $stdin.readlines
     end
   end
 end
---
+++ mrblib/grokly/commands/help.rb
@@ -0,0 +1,17 @@
+module Grokly
+  module Commands
+    class Help
+      def run
+        puts "gorkly #{Grokly::VERSION}"
+        puts
+        puts "Usage:"
+        puts
+        puts "  grokly [option] sentence"
+        puts "      -d /path/to/dictionary\t use custom dictionary"
+        puts "      -e /path/to/examples\t check sentence against examples"
+        puts "      --help\t\t\t show this message"
+        puts "      --version\t\t\t print grokly version"
+      end
+    end
+  end
+end
